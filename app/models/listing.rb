fa69a806825617fccc94e0a44537020d5316860d
artariq <alirtariq@gmail.com>
2014-05-04 01:55:49 -0400

    only save images onto dropbox when on heroku

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,12 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+		validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png image/gif)
+	else	
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
 					    :storage => :dropbox,
-					    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-	validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png image/gif)
+					    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+					    :path => ":style/:id_:filename"
+		validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png image/gif)
+	end
 end
